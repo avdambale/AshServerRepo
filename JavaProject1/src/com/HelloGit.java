66c188cab231c01a6a67be0c0ef1537ea3207237
Akash 2 <Akash@192.168.0.23>
2017-11-18 17:31:35 +0530

    added hi 6 7 8

--- JavaProject1/src/com/HelloGit.java
+++ JavaProject1/src/com/HelloGit.java
@@ -5,6 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 	
 System.out.println("Hi 1");
+System.out.println("Hi 6");
+System.out.println("Hi 7");
+System.out.println("Hi 8");
 	}
 
 }
